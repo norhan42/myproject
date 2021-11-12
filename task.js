9795b69a6543cddee3caba28892d519d383ade07
norhan ibrahim <norhanibrahim650@yahoo.com>
2021-11-12 08:01:33 -0800

    first commit

---
+++ task.js
@@ -0,0 +1,9 @@
+// function mouseclicked {
+//     if (Element.classlist.contains("mouseclick") == false) {
+//         Elementref.classlist.add("mouseclick")
+//     } else {
+//         Elementref.classlist.remove("mouseclick")
+//     }
+// }
+
+document.querySelector("button").style.color = 'blue' ; 
